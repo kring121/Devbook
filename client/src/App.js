9dfb5374018f324d7b723487e1d979547b407915
Christian von Lehe <kring.vonlehe@gmail.com>
2019-01-29 15:46:08 -0500

    deployment test

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      users: [],
+    }
+  }
+  componentDidMount(){
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({users: users}));
+  }
+  render() {
+    const { users } = this.state;
+    return (
+      <div className="App">
+        <ul>
+          {users.map(user => <li key={user.id}>{user.username}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
